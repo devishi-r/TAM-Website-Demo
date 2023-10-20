93e2151d0863ffe10419ea9eaa361adee2c475c2
devishi-r <129303782+devishi-r@users.noreply.github.com>
2023-10-20 16:14:12 +0530

    code before firebase deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html"],
   theme: {
+
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+
     extend: {},
   },
   plugins: [],
